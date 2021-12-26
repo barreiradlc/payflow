2e87220b658031d96f54af270e8f6f857805d33a
Augusto Barreira <barreira266@hotmail.com>
2021-12-26 15:39:22 -0300

    Camera configs

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,13 +1,20 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:payflow/shared/themes/appcolors.dart';
 import 'modules/barcode_scanner/barcode_scanner_page.dart';
 import 'modules/home/home_page.dart';
+import 'modules/insert_boleto/insert_boleto.dart';
 import 'modules/login/login_page.dart';
 import 'modules/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key}) : super(key: key);
+  AppWidget({Key? key}) : super(key: key) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +28,7 @@ class AppWidget extends StatelessWidget {
         "/login": (context) => LoginPage(),
         "/home": (context) => HomePage(),
         "/barcode_scanner": (context) => BarcodeScannerPage(),
+        "/insert_boleto": (context) => InsertBoletoPage(),
       },
     );
   }
--- lib/shared/widgets/set_label_buttons/set_label_buttons.dart
+++ lib/shared/widgets/set_label_buttons/set_label_buttons.dart
@@ -1,31 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/themes/appcolors.dart';
 import 'package:payflow/shared/widgets/divider_widget/divider_widget_vertical.dart';
 import 'package:payflow/shared/widgets/label_button/label_button.dart';
 
 class SetLabelButtons extends StatelessWidget {
-  const SetLabelButtons({Key? key}) : super(key: key);
+  final String primaryLabel;
+  final VoidCallback primaryOnPressed;
+  final String secondaryLabel;
+  final VoidCallback secondaryOnPressed;
+  final bool enablePrimaryColor;
+  const SetLabelButtons(
+      {Key? key,
+      required this.primaryLabel,
+      required this.primaryOnPressed,
+      required this.secondaryLabel,
+      required this.secondaryOnPressed,
+      this.enablePrimaryColor = false})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: AppColors.shape,
       height: 56,
       child: Row(
         children: [
           Expanded(
             child: LabelButton(
-              label: "Inserir código do boleto",
-              onPressed: () {
-                print("AAAAAAA");
-              },
+              label: primaryLabel,
+              onPressed: primaryOnPressed,
+              style: enablePrimaryColor ? TextStyles.buttonPrimary : null,
             ),
           ),
           DividerVertical(),
           Expanded(
             child: LabelButton(
-              label: "Adicionar da galeria",
-              onPressed: () {
-                print("AAAAAAA");
-              },
+              label: secondaryLabel,
+              onPressed: secondaryOnPressed,
             ),
           ),
         ],
